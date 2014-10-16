85cc804a3ff6e3cddb0e3913f2ea500f5647d7a6
herloct <herloct@gmail.com>
2014-10-16 20:26:23 +0700

    Create PingCommand

---
+++ src/Loct/Pinger/Command/PingCommand.php
@@ -0,0 +1,20 @@
+<?php
+namespace Loct\Pinger\Command;
+
+use \Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PingCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('ping')
+            ->setDescription('Ping some host and send notification if the host unreached');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('All host has been successfully ping-ed');
+    }
+}
