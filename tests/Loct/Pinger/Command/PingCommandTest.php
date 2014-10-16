1273994cd4a08c8cc435838d92bcf66d28b7d541
herloct <herloct@gmail.com>
2014-10-16 20:25:46 +0700

    Create testcase

---
+++ tests/Loct/Pinger/Command/PingCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+use \Loct\Pinger\Command\PingCommand;
+use \Symfony\Component\Console\Application;
+use \Symfony\Component\Console\Tester\CommandTester;
+use \PHPUnit_Framework_TestCase;
+
+class PingCommandTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testExecuteIsSucces()
+    {
+        $application = new Application();
+        $application->add(new PingCommand());
+
+        $command = $application->find('ping');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName()]);
+
+        $this->assertRegExp('/All host has been successfully ping-ed/', $commandTester->getDisplay());
+    }
+}
