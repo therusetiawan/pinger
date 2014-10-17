ae70d9fbfff9af96dbd2d5a7176827029d220605
herloct <herloct@gmail.com>
2014-10-17 23:29:33 +0700

    Isolate console into dedicated provider

---
+++ src/Loct/Pinger/Provider/ConsoleProvider.php
@@ -0,0 +1,36 @@
+<?php
+namespace Loct\Pinger\Provider;
+
+use \Pimple\Container;
+use \Pimple\ServiceProviderInterface;
+use \Symfony\Component\Console\Application;
+
+/**
+ * Service provider for console related classes and parameters.
+ *
+ * @author herloct <herloct@gmail.com>
+ */
+class ConsoleProvider implements ServiceProviderInterface
+{
+
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple An Container instance
+     */
+    public function register(Container $pimple)
+    {
+        $pimple['console'] = function ($pimple)
+        {
+            $command = $pimple['ping_command'];
+
+            $application = new Application();
+            $application->add($command);
+
+            return $application;
+        };
+    }
+}
