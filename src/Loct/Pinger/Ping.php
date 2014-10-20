dda56bfb40b534b6d56aaf66cc46e891b7681b28
herloct <herloct@gmail.com>
2014-10-21 01:00:32 +0700

    Create fsockopen-only Ping class with auto-extract port support

---
+++ src/Loct/Pinger/Ping.php
@@ -0,0 +1,73 @@
+<?php
+namespace Loct\Pinger;
+
+/**
+ * Ping component that only use fsockopen.
+ *
+ * @author herloct <herloct@gmail.com>
+ */
+class Ping extends \JJG\Ping
+{
+
+    /**
+     * Constructor
+     *
+     * @param string $host The host to be pinged
+     */
+    public function __construct($host)
+    {
+        parent::__construct($host, 10);
+
+        $this->extractHostAndPort($host);
+    }
+
+    /**
+     * Extract host and port.
+     *
+     * If the host doesn't include port information, then the default is 80.
+     *
+     * @param string $source
+     */
+    protected function extractHostAndPort($source)
+    {
+        $host = $source;
+        $port = 80;
+        $parts = explode(':', $source);
+
+        if (count($parts) === 2) {
+            $host = $parts[0];
+            $port = $parts[1];
+        }
+
+        $this->setHost($host);
+        $this->setPort($port);
+    }
+
+    /**
+     * Ping a host.
+     *
+     * @param string $method
+     *   Method to use when pinging:
+     *     - exec (default): Pings through the system ping command. Fast and
+     *       robust, but a security risk if you pass through user-submitted data.
+     *     - fsockopen: Pings a server on port 80.
+     *     - socket: Creates a RAW network socket. Only usable in some
+     *       environments, as creating a SOCK_RAW socket requires root privileges.
+     *
+     * @return mixed
+     *   Latency as integer, in ms, if host is reachable or FALSE if host is down.
+     */
+    public function ping($method = 'exec') {
+        $starttime = microtime(true);
+        $file = @fsockopen($this->getHost(), $this->getPort(), $errno, $errstr, $this->getTtl());
+        $stoptime = microtime(true);
+        $latency = false;
+
+        if ($file !== false) {
+            fclose($file);
+            $latency = floor(($stoptime - $starttime) * 1000);
+        }
+
+        return $latency;
+    }
+}
