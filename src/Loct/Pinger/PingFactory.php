92c4bbc87cccb82dd50df17eb2ff61298f9c6ff5
herloct <herloct@gmail.com>
2014-10-21 01:00:51 +0700

    Return new Ping class

--- src/Loct/Pinger/PingFactory.php
+++ src/Loct/Pinger/PingFactory.php
@@ -1,21 +1,19 @@
 <?php
 namespace Loct\Pinger;
 
-use \JJG\Ping;
-
 /**
- * Factory to create new JJG\Ping
- * 
+ * Factory to create new Ping
+ *
  * @author herloct <herloct@gmail.com>
  */
 class PingFactory
 {
     /**
-     * Create new JJG\Ping
-     * 
+     * Create new Ping
+     *
      * @params string  $host Host
      * @params integer $ttl  Ping ttl
-     * @return \JJG\Ping
+     * @return Loct\Pinger\Ping
      */
     public function createPing($host)
     {
